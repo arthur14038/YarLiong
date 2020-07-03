5fef9d40564b52dc338875486ee408a2d9ebbb60
arthur14038 <arthur14038@gmail.com>
2020-07-03 17:32:03 +0800

    新增CheePonView與Prefab 修改MainScene的解析度

---
+++ Assets/Scripts/ImplementMVC/CheePonView.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YarLiong.View;
+
+public class CheePonView : MonoBehaviour, ICheePonView
+{
+    public void Destroy()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Hide()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IEnumerator Init()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Show()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
