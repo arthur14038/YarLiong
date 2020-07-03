bf7ff16700268ceb590d66578c0209948d6ac847
Alice <alice830527@gmail.com>
2020-07-03 18:28:56 +0800

    新增綠豆糕view

---
+++ Assets/Scripts/ImplementMVC/LuDouGaoView.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace YarLiong.View
+{
+    public class LuDouGaoView : MonoBehaviour, ILuDouGaoView
+    {
+        [SerializeField]
+        private Button m_LuDouGanBtn = null;
+
+        public void Destroy()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Hide()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerator Init()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Show()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
