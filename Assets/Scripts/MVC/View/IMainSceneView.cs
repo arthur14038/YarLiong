4c2f7a04f9aba8eac1c3c9544f59121a78c11339
arthur14038 <arthur14038@gmail.com>
2020-07-03 17:13:01 +0800

    修改貼圖的WrapMode為Repeat YarLiongFactory支援取得MainSceneView MainSceneController實作初始化MainSceneView的功能與監聽按鈕事件 新增MainSceneView的Prefab

--- Assets/Scripts/MVC/View/IMainSceneView.cs
+++ Assets/Scripts/MVC/View/IMainSceneView.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using YarLiong.Controller;
 
 namespace YarLiong.View
 {
     public interface IMainSceneView : IView
     {
+        void SetListener(IMainSceneListener listener);
     }
 }
